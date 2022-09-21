183de39e4e371de1c852b8e5ff726f2d7a70e558
Ayman Ali <mujayman@amazon.com>
2022-09-22 00:10:16 +0100

    Added workaround for preferences route

--- lib/views/preferences/selections_1.dart
+++ lib/views/preferences/selections_1.dart
@@ -6,11 +6,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:nextmunch/views/preferences/selections_2.dart';
+import 'package:nextmunch/views/register_view.dart';
 import 'dart:developer' as devtools show log;
 import '../../constants/routes.dart';
-
+var cuisine_list = <String>{};
 class Cuisine_Selection extends StatefulWidget {
+
   const Cuisine_Selection({super.key});
+  
   // Future<void> readJson() async{
   //   final String response = await rootBundle.loadString('json_files/cuisines.json');
   //   final data = await json.decode(response);
@@ -22,10 +26,11 @@ class Cuisine_Selection extends StatefulWidget {
 } 
 
 class _Cuisine_SelectionState extends State<Cuisine_Selection> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Cuisine Selection')),
+      // appBar: AppBar(title: const Text('Cuisine Selection')),
       // body: Column(children:[const Text('SELECT CUISINES'),
       // TextButton(onPressed: () async {
       //     Navigator.of(context).pushNamedAndRemoveUntil(
@@ -38,19 +43,45 @@ class _Cuisine_SelectionState extends State<Cuisine_Selection> {
         var showData = json.decode(snapshot.data.toString());
         return ListView.builder(
           itemBuilder: (BuildContext context, int index){
-            return ListTile(
-              title: Text(showData[index]['cuisine']),
-              subtitle: Text('Test'),
+            return TextButton(
+              onPressed: () async { 
+
+                if(showData[index]['cuisine'].toString() == "BUTTON"){
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    preferences2,
+                    (route) => false,
+                    );
+                }
+                else{
+                  cuisine_list.add(showData[index]['cuisine'].toString());
+                  devtools.log(cuisine_list.toString());
+                }
+                
+
+                // setState(() => isButtonActive = false);
+                
+               },
+              child: Text(showData[index]['cuisine']),
+              style: TextButton.styleFrom( //<-- SEE HERE
+                side: BorderSide(width: 1.0),
+              ),
+          
+              
+            
             );
           },
           itemCount: showData?.length ?? 0,
         );
-      },future: DefaultAssetBundle.of(context).loadString("json_files/cuisines.json"),
-      ),
+        
+        },
+        future: DefaultAssetBundle.of(context).loadString("json_files/cuisines.json"),
+       
+        ),
                     
-      // child: const Text('Next Page'),
+        // child: const Text('Next Page'),
 
       ), 
+    
     );
   }
 }
