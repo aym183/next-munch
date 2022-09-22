acebd673f6f93f43292d45a2c088eb93cb6ebf25
Ayman Ali <mujayman@amazon.com>
2022-09-22 21:10:56 +0100

    Added preferences selection

--- lib/views/preferences/selections_2.dart
+++ lib/views/preferences/selections_2.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+import 'dart:developer' as devtools show log;
 import '../../constants/routes.dart';
+var restaurant_list = <String>{};
 
 class Restaurant_Selection extends StatefulWidget {
   const Restaurant_Selection({super.key});
@@ -15,20 +18,58 @@ class _Restaurant_SelectionState extends State<Restaurant_Selection> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Restaurants Selection')),
-      body: Column(children:[const Text('SELECT RESTAURANTS'),
-      TextButton(onPressed: () async {
-          Navigator.of(context).pushNamedAndRemoveUntil(
+      // appBar: AppBar(title: const Text('Cuisine Selection')),
+      // body: Column(children:[const Text('SELECT CUISINES'),
+      // TextButton(onPressed: () async {
+      //     Navigator.of(context).pushNamedAndRemoveUntil(
+      //               preferences2,
+      //               (route) => false,
+      //     );
+      //     },
+      body: Center(
+        child: FutureBuilder(builder: (context, snapshot){
+        var showData = json.decode(snapshot.data.toString());
+        return ListView.builder(
+          itemBuilder: (BuildContext context, int index){
+            return TextButton(
+              onPressed: () async { 
+
+                if(showData[index]['restaurant'].toString() == "BUTTON"){
+                  Navigator.of(context).pushNamedAndRemoveUntil(
                     preferences3,
                     (route) => false,
-          );
-          },
-                    
-          child: const Text('Next'),
+                    );
+                }
+                else{
+                  restaurant_list.add(showData[index]['restaurant'].toString());
+                  devtools.log(restaurant_list.toString());
+                }
+                
 
-      )
-      ],
-    ),
+                // setState(() => isButtonActive = false);
+                
+               },
+              child: Text(showData[index]['restaurant']),
+              style: TextButton.styleFrom( //<-- SEE HERE
+                side: BorderSide(width: 1.0),
+              ),
+          
+              
+            
+            );
+          },
+          itemCount: showData?.length ?? 0,
+        );
+        
+        },
+        future: DefaultAssetBundle.of(context).loadString("json_files/restaurants.json"),
+       
+        ),
+                    
+        // child: const Text('Next Page'),
+
+      ), 
+    
     );
   }
 }
--- lib/views/preferences/selections_3.dart
+++ lib/views/preferences/selections_3.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import '../../constants/routes.dart';
+var dietary_list = <String>{};
 
 class Personal_Preferences extends StatefulWidget {
   const Personal_Preferences({super.key});
@@ -12,23 +13,63 @@ class Personal_Preferences extends StatefulWidget {
 }
 
 class _Personal_PreferencesState extends State<Personal_Preferences> {
+  double budget_value = 50;
+  double distance_value = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Personal Preferences')),
-      body: Column(children:[const Text('SELECT BUDGET'),
-      TextButton(onPressed: () async {
-          Navigator.of(context).pushNamedAndRemoveUntil(
-                    preferences4,
-                    (route) => false,
-          );
-          },
-                    
-          child: const Text('Next'),
-
-      )
+      appBar: AppBar(title: const Text('Preferences'),),
+      body: Column(children: [
+        Title(color: Colors.black, child: Text('MORE ABOUT YOU')),
+        Title(color: Colors.black, child: Text('BUDGET')),
+        Slider(
+          value: budget_value,
+          min: 0,
+          max: 100,
+          activeColor: Colors.red,
+          divisions: 20,
+          label: budget_value.round().toString(),
+          onChanged: (value) => setState(() => this.budget_value = value),
+        ),
+        Title(color: Colors.black, child: Text('DISTANCE FROM YOU')),
+        Slider(
+          value: distance_value,
+          min: 1,
+          max: 20,
+          activeColor: Colors.red,
+          divisions: 20,
+          label: distance_value.round().toString(),
+          onChanged: (value) => setState(() => this.distance_value = value),
+        ),
+        Title(color: Colors.black, child: Text('Dietary Requirements')),
+        Row(children: [
+          TextButton(onPressed: (){dietary_list.add('Halal');}, child: Text('Halal')), 
+          TextButton(onPressed: (){dietary_list.add('Vegan');}, child: Text('Vegan')), 
+          TextButton(onPressed: (){dietary_list.add('Vegatarian');}, child: Text('Vegatarian')), 
+          TextButton(onPressed: (){dietary_list.add('Kosher');}, child: Text('Kosher')), 
+          TextButton(onPressed: (){dietary_list.add('Pescatarian');}, child: Text('Pescatarian')), 
+        ],),
+        TextButton(onPressed: (){
+          print(dietary_list);
+          print(distance_value);
+          print(budget_value);
+        }, child: Text('NEXT')), 
       ],
-    ),
+      )
+      
+    //   body: Column(children:[const Text('SELECT BUDGET'),
+    //   // TextButton(onPressed: () async {
+    //   //     Navigator.of(context).pushNamedAndRemoveUntil(
+    //   //               preferences4,
+    //   //               (route) => false,
+    //   //     );
+    //   //     },
+                    
+    //   //     child: const Text('Next'),
+
+    //   // )
+    //   ],
+    // ),
     );
   }
 }
