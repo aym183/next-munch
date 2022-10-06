ac7bdcb120a087bc6bd8faae8f6dd0acf3dfbb43
Ayman Ali <mujayman@amazon.com>
2022-10-06 01:30:27 +0100

    Added ratings and num of reviews to datset

---
+++ lib/scripts/Algo.py
@@ -0,0 +1,6 @@
+import numpy as np
+import difflib
+# import sklearn
+from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
